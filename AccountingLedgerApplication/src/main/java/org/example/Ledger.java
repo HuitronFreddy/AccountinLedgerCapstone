af4aa4903c3ddf43b8bb8520f7264e06c633d3f6
HuitronFreddy <freddy.huitron.tfi@gmail.com>
2023-10-24 14:43:11 -0500

    ClassUpdate

--- AccountingLedgerApplication/src/main/java/org/example/Ledger.java
+++ AccountingLedgerApplication/src/main/java/org/example/Ledger.java
@@ -63,36 +63,4 @@ public class Ledger {
     public void setDescription(String description) {
         this.description = description;
     }
-
-    /*static void displayAllEntries(ArrayList<Ledger> transactionList) {
-        // Sort the transactionList by date in descending order (newest to oldest)
-        Collections.sort(transactionList, Comparator.comparing(Ledger::getDate).reversed());
-        // Display all entries from transactionList
-        for (Ledger transaction : transactionList) {
-            System.out.println(transaction);
-        }
-    }
-    static void displayDeposits(ArrayList<Ledger> transactionList) {
-        // Sort again newest to oldest
-        Collections.sort(transactionList, Comparator.comparing(Ledger::getDate).reversed());
-        // Display deposits only
-        for (Ledger transaction : transactionList) {
-            if (transaction.getAmount() > 0) {
-                System.out.println(transaction);
-            }
-        }
-    }
-    static void displayPayments(ArrayList<Ledger> transactionList) {
-        // Sort again newest to oldest
-        Collections.sort(transactionList, Comparator.comparing(Ledger::getDate).reversed());
-        // Display payments or negative entries only
-        for (Ledger transaction : transactionList) {
-            if (transaction.getAmount() < 0) {
-                System.out.println(transaction);
-            }
-        }
-    }
-    static void runReport(ArrayList<Ledger> transactionList) {
-        // report method here
-    }*/
 }
